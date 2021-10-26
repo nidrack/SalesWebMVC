3ca2cf7428acb637b29eb2113373a49162f7c650
Itamar Takeo <itamar.abe@gmail.com>
2021-10-26 14:55:46 -0300

    Validação

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -10,15 +10,22 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60,MinimumLength = 2,ErrorMessage ="{0} size should be between {2} and {1}")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage ="Enter a valid email")]
         public string Email { get; set; }
         [Display(Name="Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} required")]
         public DateTime BirthDate { get; set; }
         [Display(Name = "Base Salary")]
+        [Range(100.0, 50000.0, ErrorMessage ="{0} must be from {1} to {2}")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} required")]
         public double Salary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
