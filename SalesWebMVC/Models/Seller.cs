390d606bc8bc075f05ff3e607a98de5487685db1
Itamar Takeo <itamar.abe@gmail.com>
2021-10-26 11:31:39 -0300

    Formatação de dados de saída

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -11,8 +11,14 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name="Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Salary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
